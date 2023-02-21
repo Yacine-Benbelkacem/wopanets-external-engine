d185261857562dc24c163694a490b9868f283e15
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 16:40:01 +0100

    Filled getters and setters

--- classes/Port.py
+++ classes/Port.py
@@ -1,47 +1,45 @@
 
 class Port(object):
-    def __init__(self):
-        self.switch = None
+    def __init__(self,switch=None,number=0):
+        self.switch = switch
         self.__backlog = 0
-        self.__number = 0
+        self.__number = number
         self.__delay = 0.
-        
-    # Start of user code -> properties/constructors for Port class
-
-    # End of user code
+    
+    
+    
+    
+    
     def compute_backlog(self):
         # Start of user code protected zone for compute_backlog function body
-        raise NotImplementedError
+        self.__backlog=0
         # End of user code	
+        
     def compute_delay(self):
         # Start of user code protected zone for compute_delay function body
-        raise NotImplementedError
+        self.__delay=0
         # End of user code	
-    def number(self):
-        # Start of user code protected zone for number function body
-        return 0
+        
+    def number(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__number=args[0]
+        else:
+            return self.__number
         # End of user code	
-    def number(self, number):
-        # Start of user code protected zone for number function body
-        raise NotImplementedError
-        # End of user code	
-    def backlog(self):
-        # Start of user code protected zone for backlog function body
-        return 0
-        # End of user code	
-    def backlog(self, number):
-        # Start of user code protected zone for backlog function body
-        raise NotImplementedError
-        # End of user code	
-    def delay(self):
-        # Start of user code protected zone for delay function body
-        return 0.
-        # End of user code	
-    def delay(self, number):
-        # Start of user code protected zone for delay function body
-        raise NotImplementedError
-        # End of user code	
-    # Start of user code -> methods for Port class
+        
+    def backlog(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__backlog=args[0]
+        else:
+            return self.__backlog
+        
+    def delay(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__delay=args[0]
+        else:
+            return self.__delay
 
-    # End of user code
 
