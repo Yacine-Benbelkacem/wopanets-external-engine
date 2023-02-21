93fe32cd3a03165e685230b6e365c2840716cd83
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 03:39:39 +0100

    Updated setters, getters and initialization list

--- classes/Node.py
+++ classes/Node.py
@@ -12,29 +12,31 @@ class Node(object):
     #----------------
     # Getters/setters
     #----------------
-    def name(self):
-        return self.__name
+    def name(self,*args):
+        if len(args) > 0:
+            self.__name=args[0]
+        else:
+            return self.__name
         	
-    def name(self, name):
-        self.__name=name
+    def kind(self,*args):
+        if len(args) > 0:
+            self.__kind=args[0]
+        else:
+            return self.__kind
         	
-    def kind(self):
-        return self.__kind
+    def capacity(self,*args):
+        if len(args) > 0:
+            self.__capacity=args[0]
+        else:
+            return self.__capacity
+        	
 
-    def kind(self, kind):        
-        self.__kind=kind
         	
-    def capacity(self):
-        return self.__capacity
-        	
-    def capacity(self, capacity):
-        self.__capacity=capacity
-        	
-    def service_policy(self):
-        return self.__service_policy
-        	
-    def service_policy(self, service_policy):
-        self.__service_policy=service_policy
+    def service_policy(self,*args):
+        if len(args) > 0:
+            self.__service_policy=args[0]
+        else:
+            return self.__service_policy
         	
 
 
