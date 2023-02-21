6d6563e4559b2b1da8d971ac1c9070eba188e116
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 03:39:28 +0100

    Updated setters, getters and initialization list

--- classes/Station.py
+++ classes/Station.py
@@ -1,7 +1,20 @@
-from Node import *
+from .Node import *
 class Station(Node):
-    def __init__(self):
-        self.network = None
+    def __init__(self,
+                 name="",
+                 kind='station',
+                 service_policy="",
+                 capacity=10,
+                 network=None):
+        
+        super().__init__(name,kind,service_policy,capacity)
+        
+        if (network != None):
+            self.network = network
+            #self.network.stations.append(self)
+        else:
+            self.network=None
+        
         self.link = None
         self.flows = []
         
