0e2a2c43c31283048673ee7aa85d7210ed891029
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 03:39:59 +0100

    Updated setters, getters and initialization list

--- classes/Flow.py
+++ classes/Flow.py
@@ -1,76 +1,78 @@
 
 class Flow(object):
-    def __init__(self):
+    def __init__(self,name,source,payload,overhead,period,network):
         self.__priority = ""
         self.__eed = 0.
-        self.__overhead = 0
-        self.network = None
-        self.__name = ""
-        self.__payload = 0.
+        self.__overhead = overhead
+        self.__name = name
+        self.__payload = payload
         self.__jitter = 0.
-        self.source = None
+        self.source = source
         self.targets = []
         self.__deadline = 0.
-        self.__period = 0.
+        self.__period = period
+        
+        if (network != None):
+            self.network = network
+            #self.network.flows.append(self)
+        else:
+            self.network=None
         
     # Start of user code -> properties/constructors for Flow class
+	
+    def name(self,*args):
+        # Start of user code protected zone for name function body
+        if len(args) > 0:
+            self.__name=args[0]
+        else:
+            return self.__name
+        # End of user code	
+    def period(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__period=args[0]
+        else:
+            return self.__period
+        # End of user code	
 
-    # End of user code
-    def name(self):
-        # Start of user code protected zone for name function body
-        return ""
         # End of user code	
-    def name(self, name):
-        # Start of user code protected zone for name function body
-        raise NotImplementedError
         # End of user code	
-    def period(self):
+    def deadline(self,*args):
         # Start of user code protected zone for period function body
-        return 0.
+        if len(args) > 0:
+            self.__deadline=args[0]
+        else:
+            return self.__deadline
         # End of user code	
-    def period(self, period):
+
+    def jitter(self,*args):
         # Start of user code protected zone for period function body
-        raise NotImplementedError
+        if len(args) > 0:
+            self.__jitter=args[0]
+        else:
+            return self.__jitter
+
+    def payload(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__payload=args[0]
+        else:
+            return self.__payload
+
         # End of user code	
-    def deadline(self):
-        # Start of user code protected zone for deadline function body
-        return 0.
-        # End of user code	
-    def deadline(self, period):
-        # Start of user code protected zone for deadline function body
-        raise NotImplementedError
-        # End of user code	
-    def jitter(self):
-        # Start of user code protected zone for jitter function body
-        return 0.
-        # End of user code	
-    def jitter(self, period):
-        # Start of user code protected zone for jitter function body
-        raise NotImplementedError
-        # End of user code	
-    def payload(self):
-        # Start of user code protected zone for payload function body
-        return 0.
-        # End of user code	
-    def payload(self, period):
-        # Start of user code protected zone for payload function body
-        raise NotImplementedError
-        # End of user code	
-    def priority(self):
-        # Start of user code protected zone for priority function body
-        return ""
-        # End of user code	
-    def priority(self, period):
-        # Start of user code protected zone for priority function body
-        raise NotImplementedError
-        # End of user code	
-    def overhead(self):
-        # Start of user code protected zone for overhead function body
-        return 0
-        # End of user code	
-    def overhead(self, period):
-        # Start of user code protected zone for overhead function body
-        raise NotImplementedError
+    def priority(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__priority=args[0]
+        else:
+            return self.__priority
+        
+    def overhead(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__overhead=args[0]
+        else:
+            return self.__overhead
         # End of user code	
     # Start of user code -> methods for Flow class
 
