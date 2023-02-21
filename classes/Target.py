00a301419dedfeafb4f148cea60add02606f6c49
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 03:38:48 +0100

    Updated setters, getters and initialization list

--- classes/Target.py
+++ classes/Target.py
@@ -1,30 +1,27 @@
 
 class Target(object):
-    def __init__(self):
+    def __init__(self,name,flow):
         self.path = []
-        self.__name = ""
-        self.flow = None
+        self.__name = name
+        self.flow = flow
         self.__mode = ""
         
     # Start of user code -> properties/constructors for Target class
 
     # End of user code
-    def name(self):
-        # Start of user code protected zone for name function body
-        return ""
+    def name(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__name=args[0]
+        else:
+            return self.__name
         # End of user code	
-    def name(self, name):
-        # Start of user code protected zone for name function body
-        raise NotImplementedError
-        # End of user code	
-    def mode(self):
-        # Start of user code protected zone for mode function body
-        return ""
-        # End of user code	
-    def mode(self, name):
-        # Start of user code protected zone for mode function body
-        raise NotImplementedError
-        # End of user code	
+    def mode(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__mode=args[0]
+        else:
+            return self.__mode
     # Start of user code -> methods for Target class
 
     # End of user code
