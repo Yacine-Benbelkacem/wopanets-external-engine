99a54438efa3750d2c1b56691f84e4bfda755bed
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 03:39:44 +0100

    Updated setters, getters and initialization list

--- classes/Link.py
+++ classes/Link.py
@@ -1,11 +1,14 @@
-from Node import *
+from .Node import *
 
 class Link(Node):
     def __init__(self,
                  name="",
-                 kind="",
+                 kind='link',
                  service_policy="",
-                 capacity=10):
+                 capacity=10,
+                 frm="",
+                 to="",
+                 network=None):
         
         super().__init__(name,kind,service_policy,capacity)
         
@@ -13,72 +16,74 @@ class Link(Node):
         self.__direct_usage = 0.
         self.__from_port = 0
         self.__to_port = 0
-        self.__frm = ""
-        self.__to = ""
-        self.network = None
+        self.__frm = frm
+        self.__to = to
         self.__reverse_load = 0.
         self.__direct_load = 0.
         self.switches = []
         self.stations = []
+        
+        self.network = network
+        #self.network.links.append(self)
         
     # Start of user code -> properties/constructors for Link class
 
     # End of user code
     def compute_usage(self):
         #  compute_usage function body
-        raise NotImplementedError
+        self.__direct_usage =0
+        self.__reverse_usage =0
+        
         # End of user code	
     def compute_load(self):
         #  compute_load function body
-        raise NotImplementedError
+        self.__direct_load=0
+        self.__reverse_load=0
         # End of user code	
-    def frm(self):
-        #  from function body
-        return ""
-        # End of user code	
-    def frm(self, frm):
-        #  from function body
-        raise NotImplementedError
-        # End of user code	
-    def to(self, to):
-        #  to function body
-        raise NotImplementedError
-        # End of user code	
-    def to(self):
-        #  to function body
-        return ""
-        # End of user code	
-    def from_port(self):
-        #  from_port function body
-        return 0
-        # End of user code	
-    def from_port(self, frm):
-        #  from_port function body
-        raise NotImplementedError
-        # End of user code	
-    def to_port(self):
-        #  to_port function body
-        return 0
-        # End of user code	
-    def to_port(self, from):
-        #  to_port function body
-        raise NotImplementedError
-        # End of user code	
+    def frm(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__frm=args[0]
+        else:
+            return self.__frm
+    
+    def to(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__to=args[0]
+        else:
+            return self.__to	
+
+    def from_port(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__from_port=args[0]
+        else:
+            return self.__from_port
+
+        
+    def to_port(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__to_port=args[0]
+        else:
+            return self.__to_port
+        
     def direct_load(self):
         #  direct_load function body
-        return 0.
+        return self.__direct_load
         # End of user code	
     def reverse_load(self):
         #  reverse_load function body
-        return 0.
+        return self.__reverse_load
         # End of user code	
     def direct_usage(self):
         #  direct_usage function body
-        return 0.
+        return self.__direct_usage
         # End of user code	
     def reverse_usage(self):
         #  reverse_usage function body
-        return 0.
+        return self.__reverse_usage
         # End of user code	
     # Start of user code -> methods for Link class
 
