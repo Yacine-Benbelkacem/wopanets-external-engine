cdfb1a4ffccda68255ee4f50df5146835f8880d8
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-21 03:39:16 +0100

    Updated setters, getters and initialization list

--- classes/Switch.py
+++ classes/Switch.py
@@ -1,43 +1,57 @@
 
-from Node import *
+from .Node import *
 
 class Switch(Node):
-    def __init__(self):
-        self.__latency = 0.
+    def __init__(self,
+                 name="",
+                 kind='switch',
+                 service_policy="",
+                 capacity=10,
+                 
+                 latency=0,
+                 buffer_size=0,
+                 network=None):
+        
+        super().__init__(name,kind,service_policy,capacity)
+        
+        self.__latency = latency
         self.__backlog = 0
         self.ports = []
         self.__switching_technique = ""
-        self.network = None
         self.__memory_percent = 0.
         self.links = []
-        self.__buffer_size = 0
+        self.__buffer_size = buffer_size
+        
+        if (network != None):
+            self.network = network
+            #self.network.switches.append(self)
+        else:
+            self.network=None
         
     # Start of user code -> properties/constructors for Switch class
 
     # End of user code
     def compute_total_backlog(self):
         # Start of user code protected zone for compute_total_backlog function body
-        raise NotImplementedError
+        self.__backlog = 0
         # End of user code	
     def compute_memory_percent(self):
         # Start of user code protected zone for compute_memory_percent function body
-        raise NotImplementedError
+        self.__memory_percent = 0
         # End of user code	
-    def latency(self):
-        # Start of user code protected zone for latency function body
-        return 0.
+    def latency(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__latency=args[0]
+        else:
+            return self.__latency
         # End of user code	
-    def latency(self, latency):
-        # Start of user code protected zone for latency function body
-        raise NotImplementedError
-        # End of user code	
-    def backlog(self):
-        # Start of user code protected zone for backlog function body
-        return 0
-        # End of user code	
-    def backlog(self, latency):
-        # Start of user code protected zone for backlog function body
-        raise NotImplementedError
+    def backlog(self,*args):
+        # Start of user code protected zone for period function body
+        if len(args) > 0:
+            self.__backlog=args[0]
+        else:
+            return self.__backlog
         # End of user code	
     # Start of user code -> methods for Switch class
 
