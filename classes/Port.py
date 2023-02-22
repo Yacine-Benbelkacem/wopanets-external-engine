e1c4ecca6c6d56a7d1feb7f2e39156962840c7fa
BEN-BELKACEM Yacine <yacine.ben-belkacem@student.isae-supaero.fr>
2023-02-22 22:54:11 +0100

    Added curve calculations

--- classes/Port.py
+++ classes/Port.py
@@ -1,4 +1,5 @@
 
+from .AffineCurve import *
 class Port(object):
     def __init__(self,number=0,device=None,link=None):
         self.__device = device
@@ -6,9 +7,12 @@ class Port(object):
         self.__backlog = 0
         self.__number = number
         self.__delay = 0.
+        self.__arrival_curve = None
+        self.__departure_curve=None
+        self.__arrival_computed = False
+        self.__departure_computed = False
+    
 
-    
-    
     
     
     
@@ -19,11 +23,88 @@ class Port(object):
         
     def compute_delay(self):
         # Start of user code protected zone for compute_delay function body
-        if not(self.__device.is_switch()):
-            for f in self.__device.flows:
-                self.__delay += (f.payload() + f.overhead())*8/self.__device.capacity()
+        # if not(self.__device.is_switch()):
+        #     for f in self.__device.flows:
+        #         self.__delay += (f.payload() + f.overhead())*8/self.__device.capacity()
+        
+        # # theorem 1 calculate delay     
+        self.__delay = self.__arrival_curve.b()/self.__device.capacity()
         # End of user code	
         
+    
+    def compute_arrival_curve(self):
+        # the arrival curve on one port (in a switch for instance) is
+        # the sum of departure curves from external ports passing by this port
+        #
+        # we have to check the flows in which this port is a from_port, indeed
+        # if the current port is a 'from_port' for a flow it means that this flow
+        # passes through this port
+        #
+        # Once we spotted the flows passing through this port we have to retrace
+        # the departure curves until the source 
+        
+        # so now if the current port is a station's port and this station is a source
+        # of the current flow  then the arrival_curve is the rate and burst of the flow
+        
+        arrival = AffineCurve(0,0)
+        if not(self.__arrival_computed):
+            
+            if not(self.__device.is_switch()):
+                for f in self.__device.flows:
+                    arrival += AffineCurve(f.burst(),f.rate())
+            
+            
+            else: #(if it is a switch)
+                for f in self.__device.flows:
+                    for tg in f.targets:
+                        if self.is_from_port(f): #the port is a dispatcher of the flow 
+                            
+                            previous_node_index = tg.path.index(self.__device)-1 
+                                
+                            arrival += tg.path[previous_node_index].compute_departure_curve()
+                                
+                        break        
+        
+            self.__arrival_computed = True
+            self.__arrival_curve = arrival
+            
+        return self.__arrival_curve
+    
+    
+    
+    def compute_departure_curve(self):
+        
+        if not(self.__departure_computed):
+            if self.__arrival_computed:
+                # # theorem 1 calculate delay
+                self.compute_delay()
+                # theorem 2 delay the arrival curve
+                self.__departure_curve = self.__arrival_curve.applyDelay(self.__delay)
+                self.__departure_computed = True
+                return self.__departure_curve
+            
+            else:
+                self.compute_arrival_curve()
+                self.compute_departure_curve()
+        
+        return self.__departure_curve
+    
+    def neighbour_device(self):        
+        if self.__link.to() == self.__device:
+            return self.__link.frm()
+        else:
+            return self.__link.to()              
+                
+
+    def is_from_port(self,tg):
+            neighbour = self.neighbour_device()
+            if (neighbour in tg.path) and (tg.path.index(self.__device) <  tg.path.index(  neighbour  )) :    
+                return True
+            else:
+                return False
+            
+
+    
     def number(self,*args):
         # Start of user code protected zone for period function body
         if len(args) > 0:
